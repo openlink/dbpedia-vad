6509b9cdcbf59d459aa5e6d4db5100f5ec432619
Patrick van Kleef <pkleef@openlinksw.com>
2023-05-12 23:39:43 +0200

    Added support for displaying custom dbpedia datatypes

--- sql/description.sql
+++ sql/description.sql
@@ -53,6 +53,7 @@ create procedure dbp_ldd_set_ns_decl ()
     dbp_domain || '/resource/' || dbp_template || ':', 		'template-' || dbp_lang,
     dbp_domain || '/datatype/', 				'type-'     || dbp_lang,
 
+    'http://dbpedia.org/datatype/',				'dbd',
     'http://dbpedia.org/ontology/',				'dbo',
     'http://dbpedia.org/property/',				'dbp',
     'http://dbpedia.org/resource/',				'dbr',
@@ -739,7 +740,8 @@ again:
        http (sprintf ('<span %s>', rdfa));
        http_value (charset_recode (_object, 'UTF-8', '_WIDE_'));
        http ('</span>');
-       --lang := '';
+       if ((lang is NULL or lang = '') and rdfs_type <> 'http://www.w3.org/2001/XMLSchema#string')
+         lang := rdfs_type_short;
      }
    else if (__tag (_object) = 211)
      {
